14cc2387ceb282dea5daa0378e26306b50953c89
Serhat Karaman <serhat.karaman@mera.com>
2023-04-11 17:21:20 +0300

    initial commit

---
+++ src/main/java/com/serhatkaraman/sozlukreplica/SozlukReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.serhatkaraman.sozlukreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SozlukReplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SozlukReplicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhatkaraman/sozlukreplica/SozlukReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatkaraman.sozlukreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SozlukReplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
